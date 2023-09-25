4a55e44aff76a2bba9c2ee8519ff0fbd950f8c0a
Vinicius Caique <viniciuscaiquesilva@gmail.com>
2023-09-25 10:14:32 -0300

    arrumei algumas coisas

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -6,24 +6,26 @@ import { ScrollView, Text, View } from "react-native";
 export default function Home() {
     return(
         <ScrollView
-            showsVerticalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ flexGrow: 1 }}
         >
-            <View style={{ flex: 1, alignItems: "center", justifyContent: "center", borderWidth: 2, height: 600 }}>
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "space-evenly", borderWidth: 1, backgroundColor: "rgb(24 24 27)",  }}>
+
                 <View style={{ alignItems: "center", justifyContent: "center", borderWidth: 1 }}>
-                    <Text>16 Apr 2023</Text>
-                    <Text>Amsterdam, Netherlands</Text>
+                    <Text style={{ color: "white", }}>16 Apr 2023</Text>
+                    <Text style={{ color: "white", }}>Amsterdam, Netherlands</Text>
                 </View>
                 <View style={{ alignItems: "flex-start", margin: '10%', borderWidth: 1,  }}>
-                    <Text>28°</Text>
-                    <Text>Cloudy</Text>
+                    <Text style={{ color: "white", }}>28°</Text>
+                    <Text style={{ color: "white", }}>Cloudy</Text>
                     <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-evenly", marginTop: '20%', borderWidth: 1 }}>
-                        <Text>14.98 Km</Text>
-                        <Text>29.6°</Text>
-                        <Text>59%</Text>
+                        <Text style={{ color: "white", }}>14.98 Km</Text>
+                        <Text style={{ color: "white", }}>29.6°</Text>
+                        <Text style={{ color: "white", }}>59%</Text>
                     </View>
                 </View>
-                <View style={{ alignItems: "flex-start"}}>
-                    <Text>AQI</Text>
+                <View style={{ alignItems: "flex-start", }}>
+                    <Text style={{ fontWeight: "bold", color: "white", margin: 6 }}>AQI</Text>
                     <View style={{ }}>
                         <View style={{ flexDirection: "row", justifyContent: "space-between", borderWidth: 1, borderRadius: 16, borderColor: "darkgreen", backgroundColor: "darkgreen", padding: 12, width: 200 }}>
                             <Text style={{ color: "mediumspringgreen", fontWeight: "bold", fontSize: 26 }}>36</Text>
@@ -34,17 +36,26 @@ export default function Home() {
                     </View>
                 </View>
                 <View style={{  }}>
-                    <Text>Today</Text>
-                    <View style={{ flexDirection: "row" }}>
-                        <Text>16°</Text>
-                        <Text>1:00 am</Text>
-                        <Text>16°</Text>
-                        <Text>1:00 am</Text>
-                        <Text>16°</Text>
-                        <Text>1:00 am</Text>
+                    <Text style={{ fontWeight: "bold", color: "white", margin: 6 }}>Today</Text>
+                    <View style={{ flexDirection: "row", gap: 6 }}>
+                        <View style={{ borderWidth: 1, borderColor: "rgb(39 39 42)", alignItems: "center", backgroundColor: "rgb(39 39 42)", padding: 12, paddingVertical: 24, borderRadius: 12 }}>
+                            <Text style={{ color: "white" }}>16°</Text>
+                            <Text style={{ color: "white" }}>1:00 am</Text>
+                        </View>
+                        <View style={{ borderWidth: 1, borderColor: "rgb(39 39 42)", alignItems: "center", backgroundColor: "rgb(39 39 42)", padding: 12, paddingVertical: 24, borderRadius: 12 }}>
+                            <Text style={{ color: "white" }}>16°</Text>
+                            <Text style={{ color: "white" }}>1:00 am</Text>
+                        </View>
+                        <View style={{ borderWidth: 1, borderColor: "rgb(39 39 42)", alignItems: "center", backgroundColor: "rgb(39 39 42)", padding: 12, paddingVertical: 24, borderRadius: 12 }}>
+                            <Text style={{ color: "white" }}>16°</Text>
+                            <Text style={{ color: "white" }}>1:00 am</Text>
+                        </View>
                     </View>
+
+
                 </View>
-            </View>
+        </View>
         </ScrollView>
+
     )
 }
